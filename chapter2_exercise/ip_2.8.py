631781f3ec8363bed4a7be29446400bc561a4e6f
ZzzwyPIN <ZzzwyPIN@foxmail.com>
2017-12-18 20:57:21 +0800

    chapter2

---
+++ chapter2_exercise/ip_2.8.py
@@ -0,0 +1,7 @@
+print(5+3)
+print(9-1)
+print(2 * 4)
+print(16 / 2)
+x = 8
+message = "My faverat naumber is "+str(x)+"!"
+print(message)
---
+++ chapter2_exercise/mingyan.py
@@ -0,0 +1,5 @@
+sentence = ' once said,"A person who ... new."'
+name = "albert einstein"
+print(name.title()+sentence)
+name1 = "\tSmith Cooper\t"
+print(name1.strip())
